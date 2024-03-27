6153b19f746761599cf7dcc48f1ed7e9f9b37742
Mansi M. Shah <mmshah1990@gmail.com>
2024-03-27 17:51:14 +0530

    Initial commit

---
+++ calculator.rb
@@ -0,0 +1,38 @@
+class Calculator
+  ERROR_MSG = "negative numbers not allowed"
+
+  def add numbers
+    result = []
+
+    numbers.each do |num|
+      total = validate_input(num)
+      result << total.inject(&:+)
+    end
+
+    result
+  end
+
+  def validate_input num
+    input_values = num.split(/[\n,]/).map(&:to_i)
+
+    input_values
+  end
+
+end
+
+puts "======= Enter string numbers - Press 0 to exit ======"
+numbers = Array.new
+quit_input = ""
+
+while quit_input != "0"
+  numbers << gets.chomp
+  quit_input = numbers.last
+end
+
+last_element = numbers.pop
+
+calculator = Calculator.new
+result = calculator.add(numbers)
+puts
+puts "====== Output ======="
+puts result
